16c91c11adb9b59c8bc2f3ea97df8c7a87e74e1f
Evelin Maripuu <evelin.marit@gmail.com>
2025-05-25 17:27:04 +0300

    Styling, Styling the Number Input Element and configuring the TextInput Field, p 50-52

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
+Tegemist on Udemi 70 tunnise kursuse Section 4-ga: https://www.udemy.com/course/react-native-the-practical-guide/learn/lecture/. 
+Expo, react-native, node. 
+telefonis alla laadida äpp Expo Go.
+Projekti tulemuseks mobiilirakendus, numbrite 1-99 äraarvamise mäng.
+Mäng käivitub terminalis käsuga: npm start.
+
 Creating custom Buttons. Button tegemiseks guugelda github react native, vali sealt otsi button`it. Vastuseks: https://github.com/facebook/react-native/blob/main/packages/react-native/Libraries/Components/Button.js, kus näited buttonitest.
+
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,46 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-    return <View>
-        <TextInput />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
-    </View>
+	return (
+		<View style={styles.inputContainer}>
+			<TextInput 
+                style={styles.numberInput} 
+                maxLength={2} 
+                keyboardType="number-pad" 
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+			<PrimaryButton>Reset</PrimaryButton>
+			<PrimaryButton>Confirm</PrimaryButton>
+		</View>
+	);
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+	inputContainer: {
+        marginTop: 100,
+        marginHorizontal: 24,
+		padding: 16,
+        backgroundColor: '#72063c',
+        borderRadius: 6, // corner
+        elevation: 4, // Android-specific to add shadow
+        shadowColor: 'black', // the following "shadow..." are iOS-specific 
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+	},
+    numberInput: {
+        height: 50,
+        width: 80,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    }
+});
