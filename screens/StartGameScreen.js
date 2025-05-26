94fac91c3f71cf566d842d6e203f2d3df65f9e12
Evelin Maripuu <evelin.marit@gmail.com>
2025-05-26 21:35:39 +0300

    Getting Started with Game Logic, Handling User Input and Showing an Alert Dialog, p 58-59

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,33 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
+	const [enteredNumber, setEnteredNumber] = useState("");
+
+	function numberInputHandler(enteredText) {
+		setEnteredNumber(enteredText);
+	}
+
+    function resetInputNumber() {
+        setEnteredNumber('');
+    }
+
+	function confirmInputHandler() {
+		const chosenNumber = Number.parseInt(enteredNumber);
+
+		if (Number.isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+			Alert.alert(
+				"Invalid number!",
+				"Number has to be a number between 1 and 99.",
+				[{ text: 'Okay', style: 'destructive', onPress: resetInputNumber }],
+			);
+			return;
+		}
+
+        console.log('Valid number!')
+	}
+
 	return (
 		<View style={styles.inputContainer}>
 			<TextInput
@@ -10,13 +36,15 @@ function StartGameScreen() {
 				keyboardType="number-pad"
 				autoCapitalize="none"
 				autoCorrect={false}
+				onChangeText={numberInputHandler}
+				value={enteredNumber}
 			/>
 			<View style={styles.buttonsContainer}>
 				<View style={styles.buttonContainer}>
-					<PrimaryButton>Reset</PrimaryButton>
+					<PrimaryButton onPress={resetInputNumber}>Reset</PrimaryButton>
 				</View>
 				<View style={styles.buttonContainer}>
-					<PrimaryButton>Confirm</PrimaryButton>
+					<PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
 				</View>
 			</View>
 		</View>
@@ -54,7 +82,7 @@ const styles = StyleSheet.create({
 	buttonsContainer: {
 		flexDirection: "row",
 	},
-    buttonContainer: {
-        flex: 1
-    }
+	buttonContainer: {
+		flex: 1,
+	},
 });
