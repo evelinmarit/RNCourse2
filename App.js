a1040f4c8f8ff156a8f0bb48a0960ed5f98030ac
Evelin Maripuu <evelin.marit@gmail.com>
2025-06-01 23:11:08 +0300

    Creating, Using and Displaying Random Numbers, p 65

--- App.js
+++ App.js
@@ -20,14 +20,17 @@ export default function App() {
 	}
 
 	return (
-		<LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
+		<LinearGradient
+			colors={[Colors.primary700, Colors.accent500]}
+			style={styles.rootScreen}
+		>
 			<ImageBackground
 				source={require("./assets/images/background.png")}
 				resizeMode="cover"
 				style={styles.rootScreen}
 				imageStyle={styles.backgroundImage}
 			>
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>				
+				<SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
 			</ImageBackground>
 		</LinearGradient>
 	);
