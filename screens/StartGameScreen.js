f0178abc1b49e935554fe94dc383be44fa277ef2
Evelin Maripuu <evelin.marit@gmail.com>
2025-06-01 17:35:56 +0300

    Managing Colors Globally, p 64

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+import Colors from "../constants/colors";
 
 function StartGameScreen({onPickNumber}) {
 	const [enteredNumber, setEnteredNumber] = useState("");
@@ -60,7 +61,7 @@ const styles = StyleSheet.create({
 		marginTop: 100,
 		marginHorizontal: 24,
 		padding: 16,
-		backgroundColor: "#3b021f",
+		backgroundColor: Colors.primary800,
 		borderRadius: 6, // corner
 		elevation: 4, // Android-specific to add shadow
 		shadowColor: "black", // the following "shadow..." are iOS-specific
@@ -72,9 +73,9 @@ const styles = StyleSheet.create({
 		height: 50,
 		width: 80,
 		fontSize: 32,
-		borderBottomColor: "#ddb52f",
+		borderBottomColor: Colors.accent500,
 		borderBottomWidth: 2,
-		color: "#ddb52f",
+		color: Colors.accent500,
 		marginVertical: 8,
 		fontWeight: "bold",
 		textAlign: "center",
