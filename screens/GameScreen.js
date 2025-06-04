a4b0e86a57ceef04695cd741384bd9f67ad461e0
Evelin Maripuu <evelin.marit@gmail.com>
2025-06-04 21:00:22 +0300

    Using "Cascading Styles", p 69

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -20,49 +20,60 @@ let minBoundary = 1;
 let maxBoundary = 100;
 
 function GameScreen({ userNumber, onGameOver }) {
-    const initialGuess = generateRandomBetween(
-        1, 
-        100, 
-        userNumber
-    );
-    const [currentGuess, setCurrentguess] = useState(initialGuess);
+	const initialGuess = generateRandomBetween(1, 100, userNumber);
+	const [currentGuess, setCurrentguess] = useState(initialGuess);
 
-    useEffect(() => {
-        if (currentGuess === userNumber) {
-            onGameOver();
-        }
-    }, [currentGuess, userNumber, onGameOver]);
+	useEffect(() => {
+		if (currentGuess === userNumber) {
+			onGameOver();
+		}
+	}, [currentGuess, userNumber, onGameOver]);
 
-    function nextGuessHandler(direction) { // direction => 'lower', 'greader'
-        if (
-            (direction === 'lower' && currentGuess < userNumber) || 
-            (direction === 'greater' && currentGuess > userNumber)
-        ) {
-            Alert.alert("Don`t lie!", 'You know that this is wrong...', [
-                { text: 'Sorry!', style: 'cancel'},
-            ]);
-            return;
-        }
+	function nextGuessHandler(direction) {
+		// direction => 'lower', 'greader'
+		if (
+			(direction === "lower" && currentGuess < userNumber) ||
+			(direction === "greater" && currentGuess > userNumber)
+		) {
+			Alert.alert("Don`t lie!", "You know that this is wrong...", [
+				{ text: "Sorry!", style: "cancel" },
+			]);
+			return;
+		}
 
-        if (direction === 'lower') {
-            maxBoundary = currentGuess;
-        } else {
-            minBoundary = currentGuess + 1;
-        }
-        const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
-        setCurrentguess(newRndNumber);     
-    }
+		if (direction === "lower") {
+			maxBoundary = currentGuess;
+		} else {
+			minBoundary = currentGuess + 1;
+		}
+		const newRndNumber = generateRandomBetween(
+			minBoundary,
+			maxBoundary,
+			currentGuess,
+		);
+		setCurrentguess(newRndNumber);
+	}
 
 	return (
 		<View style={styles.screen}>
 			<Title>Opponent`s Guess</Title>
 			<NumberContainer>{currentGuess}</NumberContainer>
 			<Card>
-				<InstructionText>Higher or lower?</InstructionText>
-                <View>
-				<PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
-                <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
-                </View>
+				<InstructionText style={styles.InstructionText}>
+					Higher or lower?
+				</InstructionText>
+				<View style={styles.buttonsContainer}>
+					<View style={styles.buttonContainer}>
+						<PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+							-
+						</PrimaryButton>
+					</View>
+					<View style={styles.buttonContainer}>
+						<PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+							+
+						</PrimaryButton>
+					</View>
+				</View>
 			</Card>
 			{/* <View>LOG ROUNDS</View> */}
 		</View>
@@ -75,5 +86,14 @@ const styles = StyleSheet.create({
 	screen: {
 		flex: 1,
 		padding: 24,
+	},
+	InstructionText: {
+		marginBottom: 12,
+	},
+	buttonsContainer: {
+		flexDirection: "row",
+	},
+	buttonContainer: {
+		flex: 1,
 	},
 });
